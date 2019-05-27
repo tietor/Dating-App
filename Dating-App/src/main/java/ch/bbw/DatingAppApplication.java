62c7675a8ae45c898b242b95212de8c6bcfa7866
5ia17jokoglin <josua.koglin@bluewin.ch>
2019-05-27 14:26:26 +0200

    Add Project to Repo

---
+++ Dating-App/src/main/java/ch/bbw/DatingAppApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingAppApplication.class, args);
+	}
+
+}
